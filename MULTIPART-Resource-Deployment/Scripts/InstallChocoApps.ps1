c03c488f3bc3a5dfa68b904dd48dfb5db1225779
wallenc <45206965+wallenc@users.noreply.github.com>
2019-04-05 12:10:35 -0400

    Update InstallChocoApps.ps1

--- MULTIPART-Resource-Deployment/Scripts/InstallChocoApps.ps1
+++ MULTIPART-Resource-Deployment/Scripts/InstallChocoApps.ps1
@@ -1,41 +1,58 @@
 param (
+        
+        [string]$agentURI
+    )
     
-    [string]$agentURI
-
-)
-
+function WriteLog {
+        Param(
+            $message
+        )
+    
+        $timestampedMessage = $("[" + [System.DateTime]::Now + "] " + $message) | % {
+            Out-File -InputObject $_ -FilePath '$env:WinDir\Temp\ProvisioningScript.log' -Append
+        }
+    }
+    
+WriteLog "Script Begin"
+    
+WriteLog "Agent URL: $agentURI"
+    
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+    
+function unzip {
+     param( [string]$ziparchive, [string]$extractpath )
+     [System.IO.Compression.ZipFile]::ExtractToDirectory( $ziparchive, $extractpath )
+    }
+    
 $agentsTemp = "C:\Temp\Agents"
-
+    
 #Create Temp Directory
 New-Item -ItemType Directory -Path $AgentsTemp -Force -Confirm: $false -Verbose
-
+    
 #Download Installers Zip File
 Invoke-WebRequest -Uri $agentURI -OutFile "$AgentsTemp\agents.zip" -Verbose
-
+    
 #Extracting Zip File
-Try{
-Expand-Archive -Path "$AgentsTemp\agents.zip" -DestinationPath $AgentsTemp -Force
-}
-Catch{
-Add-Type -assembly "system.io.compression.filesystem"
-[io.compression.zipfile]::ExtractToDirectory("$AgentsTemp\agents.zip", $agentsTemp)
-}
-
+unzip "$AgentsTemp\agents.zip" $AgentsTemp
+    
 #Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression C:\Temp\Agents\chocolatey\chocolateyInstall.ps1
-
+    
 #Assign Packages to Install
-$Packages = 'bigfixagent.9.2.6.94.nupkg', `
-    'encasesafe.7.07.nupkg', `
-    'safenet.10.2.nupkg', `
-    'wincollect.7.2.5.nupkg', `
-    'McAfeeAgent.4.8.0.2001.nupkg'
-
+$Packages = 'bigfixagent.9.5.nupkg', `
+        'encasesafe.7.07.nupkg', `
+        'mcafeeagent.5.5.nupkg', `
+        'safenet.10.2.nupkg', `
+        'wincollect.7.2.5.nupkg'
+    
 #Install Packages
 Set-Location C:\Temp\Agents
-ForEach ($PackageName in $Packages)
-{choco install $PackageName -y -dv -s .}
+    
+ForEach ($PackageName in $Packages){
+        WriteLog "Installing Package $PackageName"
+        choco install $PackageName -y -dv -s . 
+    }
 
-#Reboot
-Restart-Computer
+
+
