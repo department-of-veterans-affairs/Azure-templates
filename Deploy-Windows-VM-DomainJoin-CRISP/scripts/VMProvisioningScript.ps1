186af76b96cfbc78d3fa0ec61b677ed4d57bc20f
wallenc <45206965+wallenc@users.noreply.github.com>
2019-04-08 10:23:32 -0400

    Add files via upload

--- Deploy-Windows-VM-DomainJoin-CRISP/scripts/VMProvisioningScript.ps1
+++ Deploy-Windows-VM-DomainJoin-CRISP/scripts/VMProvisioningScript.ps1
@@ -1,17 +1,18 @@
 ﻿param (
-    
-    [string]$agentURI,
-    [string]$groupToAdd
-
+    #Location of CRISP components
+    [string]$agentURI='https://coreinteastarm.blob.core.usgovcloudapi.net/templates/ChocolateyPackages.zip?sp=r&st=2019-04-08T13:50:18Z&se=2019-04-08T21:50:18Z&spr=https&sv=2018-03-28&sig=MGNSdmwRcr9hKXFDTVoCA1Fq49e23Y2kFA4XOEZkS0o%3D&sr=b',
+    #Domain group to add to remote desktop users for Server 2016 installs
+    [string]$groupToAdd='va.gov\cldwins_ecs_admin_prod'
 )
 
+#Function to write output to log file @ C:\Windows\Temp\ProvisioningScript.log
 function WriteLog {
     Param( 
          $message
     )
 
     $timestampedMessage = $("[" + [System.DateTime]::Now + "] " + $message) | % {
-        Out-File -InputObject $_ -FilePath '$env:WinDir\Temp\ProvisioningScript.log' -Append
+        Out-File -InputObject $_ -FilePath "$env:WinDir\Temp\ProvisioningScript.log" -Append
     }
 }
 
@@ -20,8 +21,10 @@ function unzip {
     [System.IO.Compression.ZipFile]::ExtractToDirectory( $ziparchive, $extractpath )
 }
 
+#Cert URI for VA public cert chain
 $certURI="http://aia.pki.va.gov/pki/aia/va/"
 
+#Create temp directories for CRISP components and Certs
 $agentsTemp = "C:\Temp\Agents"
 $certsTemp = "C:\Temp\Certs"
 
@@ -29,6 +32,7 @@ WriteLog "Script Begin"
 
 WriteLog "Agent URL: $agentURI"
 
+#Add assembly for unzipping CRISP components
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
 #Create Temp Directory
@@ -38,13 +42,47 @@ New-Item -ItemType Directory -Path $certsTemp -Force -Confirm: $false -Verbose
 #Download Installers Zip File
 Invoke-WebRequest -Uri $agentURI -OutFile "$agentsTemp\agents.zip" -Verbose
 
-#Download Certificates
+#Download Certificates required to join a machine to the domain
 Invoke-WebRequest -Uri $certURI/VAInternalRoot.cer -OutFile "$certsTemp\VAInternalRoot.cer"
 Invoke-WebRequest -Uri $certURI/VA-Internal-S2-RCA1-v1.cer -OutFile "$certsTemp\VA-Internal-S2-RCA1-v1.cer"
 Invoke-WebRequest -Uri $certURI/VA-Internal-S2-ICA2-v1.cer -OutFile "$certsTemp\VA-Internal-S2-ICA2-v1.cer"
 Invoke-WebRequest -Uri $certURI/VA-Internal-S2-ICA1-v1.cer -OutFile "$certsTemp\VA-Internal-S2-ICA1-v1.cer"
 Invoke-WebRequest -Uri $certURI/InternalSubCA2.cer -OutFile "$certsTemp\InternalSubCA2.cer"
 Invoke-WebRequest -Uri $certURI/InternalSubCA1.cer -OutFile "$certsTemp\InternalSubCA1.cer"
+
+#Build array of certificates to import
+$certsToImport=Get-ChildItem $certsTemp
+
+#Import certs to Root store
+ForEach ($cert in $certsToImport){
+
+    write-host $cert.Name
+
+        $certDetails = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
+
+        $certDetails.Import($certsTemp+'\'+$cert.Name)
+
+        #Check to see if the certificate is already imported into the Root store
+        if (-not (Get-Childitem cert:\LocalMachine\Root | Where-Object {$_.Thumbprint -eq $certDetails.Thumbprint})) {
+
+            $store = New-Object System.Security.Cryptography.X509Certificates.X509Store "Root", "LocalMachine"
+
+            #Open the Root store in Read/Write mode
+            $store.Open("ReadWrite")
+
+            #Import the cert
+            $store.Add($certsTemp+'\'+$cert)
+
+            #Close the store
+            $store.Close()
+
+            WriteLog "$cert has been imported into the Root store"
+           }
+
+            else {
+                WriteLog "$cert was already present in the Root Certificate Store"
+           }
+    }
 
 #Extracting Zip File
 unzip "$agentsTemp\agents.zip" $agentsTemp
@@ -53,57 +91,33 @@ unzip "$agentsTemp\agents.zip" $agentsTemp
 Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression $agentsTemp\chocolatey\chocolateyInstall.ps1
 
+#Build array of CRISP components to install
 $nupkgs=Get-ChildItem $agentsTemp | where {$_.name -like '*.nupkg'}
 
-#Install Packages
+#Install CRISP components
 Set-Location $sagentsTemp
-ForEach ($pkg in $nupkgs)
-{ WriteLog "Installing Package $pkg.name"
-choco install $pkg.name -y -dv -s . }
 
-$certsToImport=Get-ChildItem $certsTemp
+#Loop through array and install CRISP
+ForEach ($pkg in $nupkgs){ 
+    WriteLog "Installing Package $pkg.name"
+    choco install $pkg.name -y -dv -s . 
+    }
 
-ForEach ($cert in $certsToImport){
+#Cleanup after install
+Remove-Item -path $certsTemp -Recurse
+Remove-Item -path $agentsTemp -Recurse
 
-write-host $cert.Name
-    
-    $certDetails = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2
-        
-    $certDetails.Import($certsTemp+'\'+$cert.Name)
-        
-    #Check to see if the certificate is already imported into the Root store
-    if (-not (Get-Childitem cert:\LocalMachine\Root | Where-Object {$_.Thumbprint -eq $certDetails.Thumbprint})) {
-            
-        $store = New-Object System.Security.Cryptography.X509Certificates.X509Store "CA", "LocalMachine"
-           
-        #Open the Root store in Read/Write mode
-        $store.Open("ReadWrite")
-            
-        #Import the cert
-        $store.Add($certDirectory+'\'+$cert)
-            
-        #Close the store
-        $store.Close()
-            
-        WriteLog "$cert has been imported into the Root store"
-       }
-       
-        else {
-            WriteLog $cert "was already present in the Intermediate Certificate Store"
-       }
-}
-
-
+#Add domain group for Server 2016 installs
 if ((Get-WmiObject -Class Win32_OperatingSystem).Version -ge "10*" -and (Get-WmiObject -Class Win32_OperatingSystem).Caption -like "*2016*") {
 
-    WriteLog "Server 2016 Detected. Adding local group members"
+        WriteLog "Server 2016 Detected. Adding local group members"
                     
-    Add-LocalGroupMember -Group 'Administrators' -Member $groupToAdd
-    Add-LocalGroupMember -Group 'Remote Desktop Users' -Member $groupToAdd
-
-    
-    elseif ((Get-WmiObject -Class Win32_OperatingSystem).Version -like "6.3*" -and (Get-WmiObject -Class Win32_OperatingSystem).Caption -like "*2012 R2*") {
-        Write-Output "Windows Server 2012 R2 detected."
-        Write-Output "Not adding any local group/accounts..."
+        Add-LocalGroupMember -Group 'Administrators' -Member $groupToAdd
+        Add-LocalGroupMember -Group 'Remote Desktop Users' -Member $groupToAdd
+        }
+                    
+ElseIf ((Get-WmiObject -Class Win32_OperatingSystem).Version -like "6.3*" -and (Get-WmiObject -Class Win32_OperatingSystem).Caption -like "*2012 R2*") {
+       WriteLog "Windows Server 2012 R2 detected."
+       WriteLog "Not adding any local group/accounts..."
     }
 }
